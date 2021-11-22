14132a08abce018dfe9102cd6938a24cbf0c78ee
NathanV <nathan.t.vik@gmail.com>
2021-11-22 18:38:42 -0500

    added post functionality for user DB

--- flask_book/server.py
+++ flask_book/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort
+from flask import Flask, abort, request
 from config import db, parse_json
 from pymongo import cursor, results
 from flask_cors import CORS
@@ -23,9 +23,26 @@ def get_users():
 
 
 #### GET USER PROFILE INFO ###
-@app.route("api/users/<id>") #Use ID 
+@app.route("/api/users/<id>") #Use ID 
 def get_user_profile(id):
     user = db.users.find_one({"_id": id})
     if not user:
         abort(404)
+    return parse_json(user)
+
+### Create new User ###
+@app.route('/api/users', methods=['POST'])
+def save_user():
+    user = request.get_json() #gets the dictionary 
+    #validations
+    if not "username" in user:
+        return parse_json({"error":"Username is required", "success":False })
+    
+    if not "email" in user:
+        return parse_json({"error":"email is required", "success":False })
+    
+    if not "password" in user:
+        return parse_json({"error":"password is required", "success":False })
+
+    db.users.insert_one(user)
     return parse_json(user)
