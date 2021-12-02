cc0fa5ab8a52d03c804e4d07e77b6accfda7ffb8
Alexandro Garcia <Alexandro.garcia@live.com>
2021-12-01 20:08:49 -0800

    end of class

--- react_book/src/components/login.jsx
+++ react_book/src/components/login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./login.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 const ServerUrl = "http://127.0.0.1:5000";
@@ -26,21 +27,22 @@ class Login extends Component {
 
 
     render() { 
-     
         return (
-        <div>
+        <div className="login-card">
             <h2>Login</h2>
 
-            
+            <div>
+                <label>Username</label>
+                <input type="text" name="username" onChange={this.handleInputChange}></input>
+            </div>
+            <div>
                 <label>Password</label>
                 <input type="password" name="password" onChange={this.handleInputChange}></input>
-
-            <label>Password</label>
-            <input type="password" name="password" onChange={this.handleInputChange}></input>
-
-            <button className="register" onClick={ this.handleLogin }>Login</button>
-
-            <Link className="nav-link" to="/register">Not a user? Create account here.</Link>
+            </div>
+            <div>
+                <button className="btn-login" onClick={ this.handleLogin }>Login</button>
+                <Link className="nav-link" to="/register">Not a user? Create account here.</Link>
+            </div>
         </div>
         );
     }
