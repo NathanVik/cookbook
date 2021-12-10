32f27cd57aac6ca5ba45eeec46900a1fb8cd1ebc
Alexandro Garcia <Alexandro.garcia@live.com>
2021-12-10 01:16:40 -0800

    updated landing page, not finished

--- react_book/src/components/landingPage.jsx
+++ react_book/src/components/landingPage.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import './landingPage.css'
 import RecipeCard from './recipeCard';
 import RecipeService from '../services/recipeService';
+
 
 class LandingPage extends Component {
     state = {
         posts: [],
-      };
+    };
 
 
     render() {
@@ -14,8 +16,16 @@ class LandingPage extends Component {
 
 
         return (
-            <div>
-                <h1>Top Recipes of The Week: {this.state.posts.length}</h1>
+            <div className="landing-page">
+                <div className="image-container">
+                    <img className="cookbook-cover" src={`${process.env.PUBLIC_URL}/assets/images/sweet-peppers.jpg`} alt="splash-image"/>
+                    <h2 className="cover-text card">Welcome to the CookBook</h2>
+                </div>
+                <div className="message card">
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/chef-hat1.png`} alt="chef-hat"/>
+                    <p>Take a look at some of our Top Recipes of the Week!</p>
+                    <p>Count: {this.state.posts.length}</p>
+                </div>
 
                 <div className="recipeCard-list">
                     {recipesToDisplay.map((obj) => (
@@ -34,5 +44,5 @@ class LandingPage extends Component {
         this.setState({ posts: data })
     }
 }
- 
+
 export default LandingPage;
