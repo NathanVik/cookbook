f3f8d16c481404f160ff1c5ccfd18c25593b643e
Alexandro Garcia <Alexandro.garcia@live.com>
2021-12-13 17:14:39 -0800

    worked on cards

--- react_book/src/components/recipeCard.jsx
+++ react_book/src/components/recipeCard.jsx
@@ -7,8 +7,10 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 class RecipeCard extends React.Component {
     render() { 
         return (
-        <div className="recipeCard">
-            <button><FontAwesomeIcon className="like-icon" icon={faHeart} /></button>
+        <div className="recipeCard card">
+            <span className="card-header">
+                <button className="btn-icon"><FontAwesomeIcon className="like-icon" icon={faHeart} /></button>
+            </span>
             <img src="#"></img>
             <div className="card-info">
                 <h2>Card Recipe Title</h2>
