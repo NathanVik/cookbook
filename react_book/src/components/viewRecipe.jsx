3e40a2ae43d5f768ea1414099a32e156a18925fa
NathanV <nathan.t.vik@gmail.com>
2021-12-21 14:07:12 -0500

    working on display for ViewRecipe

--- react_book/src/components/viewRecipe.jsx
+++ react_book/src/components/viewRecipe.jsx
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import RecipeService from '../services/recipeService';
-
+const ServerUrl = "http://127.0.0.1:5000";
 
 class ViewRecipe extends React.Component {
     state = {
-        recipe: [],
+        recipe: {},
     }
     
     render() { 
         return <div>
-                    Show Image
-                    Show Title
-                    Show Ingredients
-                    Show Directions
+                    Show Image <img src={ ServerUrl + '/static/img/' +  this.state.recipe.filename } />
+                    Show Title {this.state.recipe.title}
+                    {/* Show Ingredients {this.state.recipe.fromDetail.ingredients}
+                    Show Directions {this.state.recipe.fromDetail.directions} */}
 
         </div>;
     }
 
     async componentDidMount() {
+        console.log("component mounted")
         let service = new RecipeService()
         let myrecipe = await service.getRecipeDetail(this.props.match.params.recipeID) //this recipe ID - by props from "recipe" collection ID - pass prop in link from recipe card
         this.setState({ recipe: myrecipe })
         console.log(this.state.recipe)
+        console.log("recipe set as state")
     }
 }
  
